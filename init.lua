9e4b0e9bb720c647c4d7f5561ef2168534c2f413
MarkuBu <markus.burrer@gmail.com>
2017-05-05 14:57:56 +0200

    First commit

---
+++ init.lua
@@ -0,0 +1,296 @@
+vec3d = {}
+vec3d.__index = vec3d
+
+setmetatable(vec3d, { __call = function(_, ...) return vec3d.new(...) end })
+
+function vec3d.up()
+	return vec3d.new(0, 1, 0)
+end
+
+function vec3d.down()
+	return vec3d.new(0, -1, 0)
+end
+
+function vec3d.left()
+	return vec3d.new(-1, 0, 0)
+end
+
+function vec3d.right()
+	return vec3d.new(1, 0, 0)
+end
+
+function vec3d.forward()
+	return vec3d.new(0, 0, 1)
+end
+
+function vec3d.backward()
+	return vec3d.new(0, 0, -1)
+end
+
+function vec3d.__add(a, b)
+	if type(a) == "number" then
+		return vec3d.new(b.x + a, b.y + a, b.z + a)
+	elseif type(b) == "number" then
+		return vec3d.new(a.x + b, a.y + b, a.z + b)
+	else
+		return vec3d.new(a.x + b.x, a.y + b.y, a.z + b.z)
+	end
+end
+
+function vec3d:add(b)
+	if type(b) == "number" then
+		self.x = self.x + b
+		self.y = self.y + b
+		self.z = self.z + b
+	else
+		self.x = self.x + b.x
+		self.y = self.y + b.y
+		self.z = self.z + b.z
+	end
+end
+
+function vec3d.__sub(a, b)
+	if type(a) == "number" then
+		return vec3d.new(a - b.x, a - b.y, a - b.z)
+	elseif type(b) == "number" then
+		return vec3d.new(a.x - b, a.y - b, a.z - b)
+	else
+		return vec3d.new(a.x - b.x, a.y - b.y, a.z - b.z)
+	end
+end
+
+function vec3d:sub(b)
+	if type(b) == "number" then
+		self.x = self.x - b
+		self.y = self.y - b
+		self.z = self.z - b
+	else
+		self.x = self.x - b.x
+		self.y = self.y - b.y
+		self.z = self.z - b.z
+	end
+end
+
+function vec3d.__mul(a, b)
+	if type(a) == "number" then
+		return vec3d.new(b.x * a, b.y * a, b.z * a)
+	elseif type(b) == "number" then
+		return vec3d.new(a.x * b, a.y * b, a.z * b)
+	else
+		return vec3d.new(a.x * b.x, a.y * b.y, a.z * b.z)
+	end
+end
+
+function vec3d:mul(b)
+	if type(b) == "number" then
+		self.x = self.x * b
+		self.y = self.y * b
+		self.z = self.z * b
+	else
+		self.x = self.x * b.x
+		self.y = self.y * b.y
+		self.z = self.z * b.z
+	end
+end
+
+function vec3d.__div(a, b)
+	if type(a) == "number" then
+		return vec3d.new(a / b.x, a / b.y, a / b.z)
+	elseif type(b) == "number" then
+		return vec3d.new(a.x / b, a.y / b, a.z / b)
+	else
+		return vec3d.new(a.x / b.x, a.y / b.y, a.z / b.z)
+	end
+end
+
+function vec3d:div(b)
+	if type(b) == "number" then
+		self.x = self.x / b
+		self.y = self.y / b
+		self.z = self.z / b
+	else
+		self.x = self.x / b.x
+		self.y = self.y / b.y
+		self.z = self.z / b.z
+	end
+end
+
+function vec3d.__eq(a, b)
+	return a.x == b.x and a.y == b.y and a.z == b.z
+end
+
+-- function vec3d.__lt(a, b)
+--   return a.x < b.x or (a.x == b.x and a.y < b.y)
+-- end
+
+-- function vec3d.__le(a, b)
+-- 	return a.x <= b.x and a.y <= b.y and a.z <= b.z
+-- end
+
+function vec3d.__tostring(a)
+	return "(" .. a.x .. ", " .. a.y .. ", " .. a.z .. ")"
+end
+
+function vec3d.new(x, y, z)
+	if type(x) == "table" then
+		return setmetatable({ x = x.x or 0, y = x.y or 0, z = x.z or 0 }, vec3d)
+	else
+		return setmetatable({ x = x or 0, y = y or 0, z = z or 0 }, vec3d)
+	end
+end
+
+function vec3d.distance(a, b)
+	return (b - a):len()
+end
+
+function vec3d:distance_to(other)
+	return (other - self):len()
+end
+
+function vec3d:clone()
+	return vec3d.new(self.x, self.y, self.z)
+end
+
+function vec3d:unpack()
+	return self.x, self.y, self.z
+end
+
+function vec3d:len()
+	return math.sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
+end
+
+function vec3d:lenSq()
+	return self.x * self.x + self.y * self.y + self.z * self.z
+end
+
+function vec3d:normalize()
+	local len = self:len()
+	self.x = self.x / len
+	self.y = self.y / len
+	self.z = self.z / len
+	-- return self
+end
+
+function vec3d:normalized()
+	return self / self:len()
+end
+
+function vec3d:round()
+	self.x = math.floor(self.x + 0.5)
+	self.y = math.floor(self.y + 0.5)
+	self.z = math.floor(self.z + 0.5)
+	-- return self
+end
+
+function vec3d:rounded()
+	local x = math.floor(self.x + 0.5)
+	local y = math.floor(self.y + 0.5)
+	local z = math.floor(self.z + 0.5)
+	return vec3d(x, y, z)
+end
+
+function vec3d:floor()
+	self.x = math.floor(self.x)
+	self.y = math.floor(self.y)
+	self.z = math.floor(self.z)
+	-- return self
+end
+
+function vec3d:floored()
+	local x = math.floor(self.x)
+	local y = math.floor(self.y)
+	local z = math.floor(self.z)
+	return vec3d(x, y, z)
+end
+
+-- function vec3d:rotate(phi)
+--   local c = math.cos(phi)
+--   local s = math.sin(phi)
+--   self.x = c * self.x - s * self.y
+--   self.y = s * self.x + c * self.y
+--   return self
+-- end
+
+-- function vec3d:rotated(phi)
+--   return self:clone():rotate(phi)
+-- end
+
+-- function vec3d:perpendicular()
+--   return vec3d.new(-self.y, self.x)
+-- end
+--
+-- function vec3d:projectOn(other)
+-- 	return (self * other) * other / other:lenSq()
+-- end
+--
+-- returns a vector with a specific length
+function vec3d:set_length(l)
+	return (self:normalized() * l)
+end
+
+-- limits the length of a vector
+function vec3d:limit(l)
+	if self:len() > l then
+		return self:set_length(l)
+	end
+	return self
+end
+
+-- returns the cross product of two vectors
+function vec3d:cross(other)
+	return vec3d.new(self.y * other.z - self.z * other.y,
+		self.z * other.x - self.x * other.z,
+		self.x * other.y - self.y * other.x)
+end
+
+-- returns the dot product of two vectors
+function vec3d:dot(other)
+	return self.x * other.x + self.y * other.y + self.z * other.z
+end
+
+function vec3d:scalar_projection(a, b)
+	local ap = self - a
+	local ab = b - a
+	ab:normalize()
+	ab = ab * ap:dot(ab)
+	return (a + ab)
+end
+
+function vec3d:is_between(a, b)
+	local ab = b - a
+	local ap = self - a
+	-- local l = ap:len()
+	local dot = math.abs(ap:dot(ab))
+	-- print(a, b, self, dot)
+	if ap:len() < ab:len() and dot > 0.9 and dot < 1.1 then
+		return true
+	end
+	return false
+end
+
+-- returns the scalar triple product of three vectors
+-- function vec3d.scalar_triple(a, b, c)
+-- 	return a:dot(b:cross(c))
+-- end
+
+-- returns the angle between two 2d vectors (x,z) in radians
+-- y from a 3d vector is ignored
+function vec3d.angle_between(a, b)
+	return math.atan2(a.x * b.z - a.z * b.x, a.x * b.x + a.z * b.z)
+end
+
+-- returns yaw from a 2d vector (x, z) in radians
+-- y from a 3d vector is ignored
+function vec3d:yaw()
+	local yaw = math.atan2(-self.x, self.z)
+	if yaw >= -math.pi and yaw < 0 then
+		yaw = math.pi * 2 + yaw
+	end
+	return yaw
+end
+
+--
+local v1 = vec3d(10, 20, 2)
+local v2 = vec3d(20, 30, 5)
+print(vec3d.distance(v1, v2))
+print(v1:distance_to(v2))
---
+++ readme.md
@@ -0,0 +1,112 @@
+## vec3d is a class library for 3d vectors.
+This mod adds a 3d vector class to Minetest.
+
+### Create a new vector
+To create a new vector you have to use the method vec3d()
+
+#### Example
+
+	local v = vec3d()      -- creates the vector {x = 0, y = 0, z = 0}
+	local v = vec3d(5,3,1) -- creates the vector {x = 5, y = 3, z = 1}
+
+### Format output
+If a vector is print to the console with the print command the output is formatted
+
+#### Example
+
+	local v1 = vec3d(5, 3, 1)
+	print(v1)
+
+The output is: `(5, 3, 1)`
+
+### Misc functions
+
+#### `vec3d:clone()`
+*   Creates a copy of a vector
+
+
+	local v1 = vec3d(10, 20, 2)
+	local v2 = v1:clone()
+	v2.x = 0
+	print(v1, v2)
+
+Prints the output `(10, 20, 2)	(0, 20, 2)`
+
+#### `vec3d:unpack()`
+*   Returns the single componets of the vector
+
+
+	local v1 = vec3d(10, 20, 2)
+	local x, y, z = v1:unpack()
+
+
+### Direction helpers
+
+#### `vec3d.up`
+*   returns new unit vector pointing up (+y)
+
+#### `vec3d.down`
+*   returns new unit vector pointing down (-y)
+
+#### `vec3d.right`
+*   returns new unit vector pointing right (+x)
+
+#### `vec3d.left`
+*   returns new unit vector pointing left (-x)
+
+#### `vec3d.forward`
+*   returns new unit vector pointing forward (+z)
+
+#### `vec3d.backward`
+*   returns new unit vector pointing backward (-z)
+
+### Basic arithmetics
+This class has two methods for basic arithmetics.
+
+#### Operators
+The first uses regular math operators +, -, \*, /. The method creates a new vector as result. Both operands can be a vector or a number.
+
+#### Example
+
+	local v1 = vec3d(5, 3, 1)
+	local v2 = vec3d(2, 4, 6)
+	local v3 = v1 + v2
+	print(v3)
+
+This prints `(7, 7, 7)`
+
+	local v1 = vec3d(5, 3, 1)
+	local v2 = v1 * 5
+	print(v2)
+
+This prints `(25, 15, 5)`
+
+#### Methodes
+The second method uses a class methode to modify a vector. This method doesn't create a new vector and is slightly faster.
+
+`x` can be a vec3d or a number
+
+	vec3d:add(x)
+	vec3d:sub(x)
+	vec3d:mul(x)
+	vec3d:div(x)
+
+#### Example
+
+	local v1 = vec3d(5, 3, 1)
+	local v2 = vec3d(2, 4, 6)
+	v1:add(v2)
+	print(v1)
+
+This prints `(7, 7, 7)`
+
+### Comparing vectors
+Vectors can be compared with `==`
+
+#### Example
+
+	local v1 = vec3d(10, 10, 10)
+	local v2 = vec3d(20, 20, 20)
+	print(v1 == v2)
+
+Prints `false` to the console
